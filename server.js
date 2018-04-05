298c902139bab26845c6a330c5f49ca6272d832f
ivan3123708 <ivan3123708@gmail.com>
2018-04-05 03:58:46 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const publicPath = path.join(__dirname, 'client', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('SERVER NOW RUNNING...');
+});
