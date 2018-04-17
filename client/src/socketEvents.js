42f461248eabec0a9def59c7a310ca85e9c8d557
ivan3123708 <ivan3123708@gmail.com>
2018-04-17 06:49:53 +0200

    Refactored socketEvents file

--- client/src/socketEvents.js
+++ client/src/socketEvents.js
@@ -2,50 +2,50 @@ import socketIOClient from 'socket.io-client';
 
 const socket = socketIOClient('http://localhost:3000');
 
-export const setUserListener = (callback) => {
-  socket.on('setUser', (user) => {
-    callback(user);
-  });
+const socketOn = {
+  setUser: (callback) => {
+    socket.on('setUser', (user) => {
+      callback(user);
+    });
+  },
+  updateUsers: (callback) => {
+    socket.on('updateUsers', (users) => {
+      callback(users);
+    });
+  },
+  setRoom: (callback) => {
+    socket.on('setRoom', (room) => {
+      callback(room);
+    });
+  },
+  updateRooms: (callback) => {
+    socket.on('updateRooms', (rooms) => {
+      callback(rooms)
+    });
+  },
+  serverMessage: (callback) => {
+    socket.on('serverMessage', (message) => {
+      callback(message);
+    });
+  }
 };
 
-export const userJoinEmitter = (nickname) => {
-  socket.emit('userJoin', nickname, (err) => {
-    if (err) {
-      console.log('ERROR');
-    } else {
-      console.log(`${nickname} JOINED`);
-    }
-  });
+const socketEmit = {
+  userJoin: (userName) => {
+    socket.emit('userJoin', userName, (err) => {
+      if (err) {
+        console.log('ERROR');
+      } else {
+        console.log(`${userName} JOINED`);
+      }
+    });
+  },
+  joinRoom: (roomName) => {
+    socket.emit('joinRoom', roomName);
+  },
+  clientMessage: (text) => {
+    socket.emit('clientMessage', text);
+  }
 };
 
-export const usersUpdateListener = (callback) => {
-  socket.on('updateUsers', (users) => {
-    callback(users);
-  });
-};
-
-export const setRoomListener = (callback) => {
-  socket.on('setRoom', (room) => {
-    callback(room);
-  });
-};
-
-export const joinRoomEmitter = (room) => {
-  socket.emit('joinRoom', room);
-};
-
-export const updateRoomsListener = (callback) => {
-  socket.on('updateRooms', (rooms) => {
-    callback(rooms)
-  });
-};
-
-export const serverMessageListener = (callback) => {
-  socket.on('serverMessage', (message) => {
-    callback(message);
-  });
-};
-
-export const clientMessageEmitter = (text) => {
-  socket.emit('clientMessage', text);
-};
+export { socketOn, socketEmit };
